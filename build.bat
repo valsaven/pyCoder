8f78dc92ae55a11ba02874a9cff6f5d2e83b5c8a
Val Saven <val.saven@gmail.com>
2023-08-25 22:01:59 +0300

    Update build script

--- build.bat
+++ build.bat
@@ -1,12 +1,22 @@
-pip3 install pyinstaller
+@echo off
 
+echo(=== Check if 'dist' directory exists and remove it... ===)
 if exist .\dist rmdir .\dist /q /s
 
+echo(=== Compile main.py into a standalone executable... ===)
 pyinstaller --onefile main.py
 
+echo(=== Copy the icon to the 'dist' directory... ===)
 copy .\th06r.ico .\dist\
 
+echo(=== Copy the contents of the 'Bin' folder to 'dist\Bin'... ===)
 xcopy /E /I .\Bin .\dist\Bin
 
-mkdir .\dist\in
-mkdir .\dist\out
+echo(=== Create 'in' and 'out' directories inside 'dist' if they don't exist... ===)
+if not exist .\dist\in mkdir .\dist\in
+if not exist .\dist\out mkdir .\dist\out
+
+echo(=== Remove main.spec file... ===)
+del main.spec
+
+echo(=== Completed! ===)
